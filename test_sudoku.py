abc212a5ad21bb8f08125530c6168c9c554173f5
Subhomoy Haldar <hungrybluedev@gmail.com>
2020-01-02 20:40:44 +0530

    Add hard test case. Current time to solve ~30 seconds.

--- test_sudoku.py
+++ test_sudoku.py
@@ -49,3 +49,32 @@ class TestSudoku(TestCase):
                          " 6 4 5 | 7 9 1 | 3 2 8 ",
                          str(sudoku),
                          "Solutions do not match.")
+
+    def test_hard(self):
+        input_string = """   . . . | . . . | . . . 
+                             . . . | . 2 9 | . 8 . 
+                             . 7 4 | . . 5 | . . 2 
+                            -------+-------+-------
+                             . . . | . . . | . 1 . 
+                             9 . 3 | . . 7 | . . . 
+                             6 . 7 | . 3 . | . 4 8 
+                            -------+-------+-------
+                             . . . | . 9 . | . . 3 
+                             . 6 5 | . . . | . . . 
+                             . 4 . | 2 . . | 6 . ."""
+        sudoku = Sudoku(input_string)
+        sudoku.solve()
+        self.assertTrue(sudoku.is_solved())
+        self.assertEqual(" 2 9 1 | 8 7 4 | 5 3 6 \n"
+                         " 5 3 6 | 1 2 9 | 7 8 4 \n"
+                         " 8 7 4 | 3 6 5 | 1 9 2 \n"
+                         "-------+-------+-------\n"
+                         " 4 5 2 | 9 8 6 | 3 1 7 \n"
+                         " 9 8 3 | 4 1 7 | 2 6 5 \n"
+                         " 6 1 7 | 5 3 2 | 9 4 8 \n"
+                         "-------+-------+-------\n"
+                         " 7 2 8 | 6 9 1 | 4 5 3 \n"
+                         " 1 6 5 | 7 4 3 | 8 2 9 \n"
+                         " 3 4 9 | 2 5 8 | 6 7 1 ",
+                         str(sudoku),
+                         "Solutions do not match.")
